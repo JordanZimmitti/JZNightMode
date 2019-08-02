b10c98aaaf6924506601b3c5c7dd64a55b86cc54
Jordan Zimmitti <jordanzimmitti@gmail.com>
2019-08-01 20:06:09 -0400

    Cleaned Up The Code
    Signed-off-by: Jordan Zimmitti <jordanzimmitti@gmail.com>

--- JZNightMode/src/main/java/me/jordanzimmitti/jz_night_mode/JZNightMode.kt
+++ JZNightMode/src/main/java/me/jordanzimmitti/jz_night_mode/JZNightMode.kt
@@ -19,151 +19,131 @@ import java.util.*
  *
  * Class That Handles The Device Theme Based On The Sunset And Sunrise
  *
- * @author Jordan Zimmitti
- *
- * @version 1.0.0
+ * @author  Jordan Zimmitti
+ * @date    08-01-2019
+ * @version 2.0.0-Beta
  */
 class JZNightMode(private val context : Context,
                   var dayTheme        : Int = 0,
                   var nightTheme      : Int = 0) {
 
-    // Define And Initializes Location Value//
-    private val location = getLocation()
-
-    // Define And Initializes String Values//
+    // Define And Initializes String Value//
     private val timeZone = Calendar.getInstance().timeZone.id
 
     /**.
-     * Function That Checks If It Is Night Time
+     * Property That Checks If It Is Night Time
      *
      * @return 'true' if it is night, 'false' if day
      */
-    fun isNight(): Boolean {
+    val isNight: Boolean get() {
 
-        // When No Location Was Found//
-        if (location == null) { getError("Location"); return false }
+            // When No Location Was Found//
+            if (location == null) { showError("Location"); return false }
 
-        // Define And Instantiates SimpleDateFormat Value//
-        val dateFormat = SimpleDateFormat("HH:mm", Locale.US)
+            // Define And Instantiates SimpleDateFormat Value//
+            val dateFormat = SimpleDateFormat("HH:mm", Locale.US)
 
-        // Define And Instantiates SunriseSunsetCalculator Value//
-        val dayNight = SunriseSunsetCalculator(location, timeZone)
+            // Define And Instantiates SunriseSunsetCalculator Value//
+            val dayNight = SunriseSunsetCalculator(location, timeZone)
 
-        // Gets The Current Time, Sunrise, And Sunset//
-        val currentTime = JZTime.getCurrentTime(JZTimeFormat.MILITARY)
-        val sunrise     = dayNight.getOfficialSunriseForDate(Calendar.getInstance())
-        val sunset      = dayNight.getOfficialSunsetForDate(Calendar.getInstance())
+            // Gets The Current Time, Sunrise, And Sunset//
+            val currentTime = JZTime.getCurrentTime(JZTimeFormat.MILITARY)
+            val sunrise     = dayNight.getOfficialSunriseForDate(Calendar.getInstance())
+            val sunset      = dayNight.getOfficialSunsetForDate(Calendar.getInstance())
 
-        // Parses The Current Time, Sunrise, And Sunset//
-        val parsedCurrentTime = dateFormat.parse(currentTime)
-        val parsedSunrise     = dateFormat.parse(sunrise)
-        val parsedSunset      = dateFormat.parse(sunset)
+            // Parses The Current Time, Sunrise, And Sunset//
+            val parsedCurrentTime = dateFormat.parse(currentTime)
+            val parsedSunrise     = dateFormat.parse(sunrise)
+            val parsedSunset      = dateFormat.parse(sunset)
 
-        // Returns True If Night, False If Day//
-        return parsedCurrentTime.before(parsedSunrise) || parsedCurrentTime.after(parsedSunset)
-    }
+            // Returns True If Night, False If Day//
+            return parsedCurrentTime.before(parsedSunrise) || parsedCurrentTime.after(parsedSunset)
+        }
 
     /**.
-     * Function That Gets The Time When The Sun Rises
+     * Property That Gets The Time When The Sun Rises
      *
      * @return The sunrise time
      */
-    fun getSunrise(): String {
+    val sunrise: String get() {
 
-        // When No Location Was Found//
-        if (location == null) { getError("Location"); return "" }
+            // When No Location Was Found//
+            if (location == null) { showError("Location"); return "" }
 
-        // Define And Instantiates SunriseSunsetCalculator Value//
-        val dayNight = SunriseSunsetCalculator(location, timeZone)
+            // Define And Instantiates SunriseSunsetCalculator Value//
+            val dayNight = SunriseSunsetCalculator(location, timeZone)
 
-        // Gets The Sunrise//
-        val sunrise = dayNight.getOfficialSunriseForDate(Calendar.getInstance())
+            // Gets The Sunrise//
+            val sunrise = dayNight.getOfficialSunriseForDate(Calendar.getInstance())
 
-        // Returns The Sunrise Time//
-        return "${sunrise.substring(1)} AM"
-    }
+            // Returns The Sunrise Time//
+            return "${sunrise.substring(1)} AM"
+        }
 
     /**.
-     * Function That Gets The Time When The Sun Sets
+     * Property That Gets The Time When The Sun Sets
      *
      * @return The sunset time
      */
-    fun getSunset(): String {
+    val sunset: String get() {
 
-        // When No Location Was Found//
-        if (location == null) { getError("Location"); return "" }
+            // When No Location Was Found//
+            if (location == null) { showError("Location"); return "" }
 
-        // Define And Instantiates SunriseSunsetCalculator Value//
-        val dayNight = SunriseSunsetCalculator(location, timeZone)
+            // Define And Instantiates SunriseSunsetCalculator Value//
+            val dayNight = SunriseSunsetCalculator(location, timeZone)
 
-        // Gets The Sunset//
-        val sunset = dayNight.getOfficialSunsetForDate(Calendar.getInstance())
+            // Gets The Sunset//
+            val sunset = dayNight.getOfficialSunsetForDate(Calendar.getInstance())
 
-        // Gets The Sunset Time In Standard Time//
-        val hour   = JZTime.fullTimeToHour(sunset, JZTimeFormat.STANDARD)
-        val minute = JZTime.fullTimeToMinute(sunset)
+            // Gets The Sunset Time In Standard Time//
+            val hour   = JZTime.fullTimeToHour(sunset, JZTimeFormat.STANDARD)
+            val minute = JZTime.fullTimeToMinute(sunset)
 
-        // Returns The Sunset Time//
-        return "$hour:$minute PM"
-    }
+            // Returns The Sunset Time//
+            return "$hour:$minute PM"
+        }
 
     /**.
-     * Function That Gets The Theme Based On The Time Of Day
+     * Property That Gets The Theme Based On The Time Of Day
      *
      * @return The day or night theme
      */
-    fun getTheme(): Int {
+    val theme: Int get() {
 
         // When dayTheme Or nightTheme Was Not Initialized//
-        if (dayTheme == 0 || nightTheme == 0) getError("No Input")
+        if (dayTheme == 0 || nightTheme == 0) showError("No Input")
 
         // Returns The Day Or Night Theme//
-        return if (isNight()) nightTheme else dayTheme
+        return if (isNight) nightTheme else dayTheme
     }
 
     /**.
-     * Function That Checks If The Location Permission Was Granted
+     * property That Checks If The Location Permission Was Granted
      *
      * @return 'true' if The permission is granted, 'false' if not
      */
-    private fun isPermissionGranted(): Boolean {
+    private val isPermissionGranted: Boolean get() {
 
-        // Define And Initializes Int Value//
-        val desiredState = PackageManager.PERMISSION_GRANTED
+            // Define And Initializes Int Value//
+            val desiredState = PackageManager.PERMISSION_GRANTED
 
-        // Define And Initializes String Value//
-        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+            // Define And Initializes String Value//
+            val permission = Manifest.permission.ACCESS_FINE_LOCATION
 
-        // Returns True If The Permission Is Granted, False If Not//
-        return ActivityCompat.checkSelfPermission(context, permission) == desiredState
+            // Returns True If The Permission Is Granted, False If Not//
+            return ActivityCompat.checkSelfPermission(context, permission) == desiredState
     }
 
     /**.
-     * Function That Shows The Appropriate Error When An Error Occurs
-     *
-     * @param [error] The error to show
-     */
-    private fun getError(error: String) {
-
-        when (error) {
-
-            // When The Error Is A Location Error//
-            "Location" -> Toast.makeText(context, "Error: Can't Retrieve Location", Toast.LENGTH_LONG).show()
-
-            // When The Error Is A Forgotten Input Error//
-            "No Input" -> error("[dayTheme] or [nightTheme] was not initialized")
-        }
-    }
-
-    /**.
-     * Function That Gets The Devices Current Location
+     * Property That Gets The Devices Current Location
      *
      * @return The Devices Location
      */
-    private fun getLocation(): Location? {
+    private val location: Location? get() {
 
         // When Device Can Not Access Location//
-        if (!isPermissionGranted()) { grantPermission(); return null }
+        if (!isPermissionGranted) { grantPermission(); return null }
 
         // Gets The Devices Location//
         val location = SimpleLocation(context)
@@ -193,6 +173,23 @@ class JZNightMode(private val context : Context,
 
             // Asks For Permission//
             ActivityCompat.requestPermissions(context as Activity, arrayOf(permission), 0)
+        }
+    }
+
+    /**.
+     * Function That Shows The Appropriate Error When An Error Occurs
+     *
+     * @param [error] The error to show
+     */
+    private fun showError(error: String) {
+
+        when (error) {
+
+            // When The Error Is A Location Error//
+            "Location" -> Toast.makeText(context, "Error: Can't Retrieve Location", Toast.LENGTH_LONG).show()
+
+            // When The Error Is A Forgotten Input Error//
+            "No Input" -> error("[dayTheme] or [nightTheme] was not initialized")
         }
     }
 }
